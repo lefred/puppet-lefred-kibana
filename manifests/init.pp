ed06bf230c20932488a768598b8b5d920497d818
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-17 16:30:19 +0200

    fix version
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class kibana ($version="1.8.1", $elasticsearch_url="127.0.0.1:9200") {
+class kibana ($version="3.1.1", $elasticsearch_url="127.0.0.1:9200") {
 
    include kibana::packages	
    include kibana::config
